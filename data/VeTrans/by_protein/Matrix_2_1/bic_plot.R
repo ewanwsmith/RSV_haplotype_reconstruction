5938a38b823ab03ee34453515971843d92006fb4
Ewan Smith <e.smith.5@research.gla.ac.uk>
2024-10-10 14:44:28 +0100

    run extract_bic.sh for other proteins

---
+++ data/VeTrans/by_protein/Matrix_2_1/bic_plot.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+
+# Read the CSV file
+bic <- read.csv("/Users/e.smith.5/Documents/PhD/RSV_project/RSV_haplotype_reconstruction/data/VeTrans/by_protein/Matrix_2_1/extracted_bic.csv")
+
+# Remove rows where BIC is NA
+bic <- bic[!is.na(bic), ]
+
+# Ensure n_haps is numeric and sort by n_haps
+bic$n_haps <- as.numeric(as.character(bic$n_haps))
+bic <- bic[order(bic$n_haps), ]
+
+# Create the plot
+bic_plot <- ggplot(bic, aes(x = n_haps, y = BIC)) +
+  geom_point() +
+  labs(title="Matrix_2_1", x="# Haplotypes", y="BIC") +
+  theme_minimal()
+
+# Save the plot
+ggsave("/Users/e.smith.5/Documents/PhD/RSV_project/RSV_haplotype_reconstruction/data/VeTrans/by_protein/Matrix_2_1/bic_plot.jpeg", plot = bic_plot, width = 8, height = 6)
+
+# Find the row with the lowest BIC
+min_bic <- bic[which.min(bic$BIC), ]
+
+# Print the file with the lowest BIC
+cat("File with the lowest BIC:", min_bic$file, "\n")
